a9a8a5cb61044aba5274718cbec59b64f66c780e
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-23 10:52:30 +0300

    user routes added

---
+++ server/routes/user.routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+// Import user controllers
+const {
+  registerUser,
+  loginUser,
+  getOwnProfile,
+  updateProfile,
+  followUser,
+  getUserProfile,
+  unfollowUser,
+} = require("../controllers/user.controllers");
+
+// User registration route
+router.post('/auth/register', registerUser);
+
+// User login route
+router.post('/auth/login', loginUser);
+
+
+// Get own profile route
+router.get('/profile', getOwnProfile);
+
+// Update profile route
+router.put('/profile', updateProfile);
+
+// Follow user route
+router.post('/follow/:userId', followUser);
+
+// Get user profile by username
+router.get('/profile/:username', getUserProfile);
+
+// Unfollow user route
+router.delete('/unfollow/:userId', unfollowUser);
+
+// Export the router
+module.exports = router;
