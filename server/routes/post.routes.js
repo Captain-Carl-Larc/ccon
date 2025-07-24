bf99f1a8d5e7782a99cd78c184c3c2e764f1c54d
Stephen Kalwenzi <kalwenzistephen2003@gmail.com>
2025-07-24 18:26:28 +0300

    created seeposts endpoint

---
+++ server/routes/post.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const AuthMiddleware = require('../middleware/auth.middleware'); // Assuming you have an auth middleware for protected routes
+const adminAuthMiddleware = require('../middleware/admin.auth.middleware'); // Middleware for admin access
+
+const {
+  createPost,
+  seeAllPosts,
+} = require('../controllers/blog.controllers');
+
+// Route to create a new post (protected route)
+router.post('/create', AuthMiddleware, createPost);
+
+// Route to see all posts (public route)
+router.get('/all', seeAllPosts);
